40ae85c4245bbe09af139d658fa27653f7aeecca
Leif Hetland <lehetland1@gmail.com>
2016-04-28 17:07:52 -0500

    basic JS101

---
+++ main.js
@@ -0,0 +1,33 @@
+// console.log("Hello from main.js");
+
+// var hoursPerYear = 365 * 24;
+// console.log(hoursPerYear);
+// var minPerDecade = hoursPerYear * 10 * 60;
+// console.log(minPerDecade); 
+// var secondsPerYear = hoursPerYear * 60 * 60;
+// var secondsOldIAm = secondsPerYear * 25;
+// console.log(secondsOldIAm);
+
+// //for loops
+
+// var num = 5
+
+// for(var i=0; i<5; i++){
+// 	console.log('Your number is:',i+1);
+// }
+
+// var myArray = ['cat','dog','fish','cow']
+
+// for(var j=0; j<4; j++){
+// 	console.log('index',j);
+// 	console.log('You are a', myArray[j])
+// }
+
+//String Manipulation
+
+var firstName = 'Leif';
+var lastName = 'Hetland';
+var name = firstName + " " + lastName;
+console.log(name);
+
+var phrase = ""
